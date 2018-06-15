6d15f3e205db45a96a77dc91280890e409bc5611
totallynotmark <40257808+totallynotmark@users.noreply.github.com>
2018-06-14 20:21:47 -0500

    Add End's Breath (pt 1/3)
    First tool!

--- src/main/java/com/minecraftpro941/moretools/init/ModItems.java
+++ src/main/java/com/minecraftpro941/moretools/init/ModItems.java
@@ -12,5 +12,6 @@ public class ModItems
 	
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
+	public static final Item ENDS_BREATH = new ItemBase("ends_breath");
 	public static final Item BOAT = new ItemBase("boat");
 }
