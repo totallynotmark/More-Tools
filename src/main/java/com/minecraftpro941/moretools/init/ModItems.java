c5cc7344654fa6b809d51aacfaf8aeb55852268b
totallynotmark <40257808+totallynotmark@users.noreply.github.com>
2018-06-14 21:04:23 -0500

    Add End's Breath

--- src/main/java/com/minecraftpro941/moretools/init/ModItems.java
+++ src/main/java/com/minecraftpro941/moretools/init/ModItems.java
@@ -4,14 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.minecraftpro941.moretools.items.ItemBase;
+import com.minecraftpro941.moretools.items.tools.ToolPickaxe;
+import com.minecraftpro941.moretools.items.tools.ToolSword;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems
 {
 	
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
-	public static final Item ENDS_BREATH = new ItemBase("ends_breath");
-	public static final Item BOAT = new ItemBase("boat");
+	//Materials
+	public static final ToolMaterial MATERIAL_DRAGONS_BREATH = EnumHelper.addToolMaterial("material_dragons_breath", 0, 1031, 0.0F, 5.0F, 15); //should have 9atk
+	
+	//Items
+	//N/A
+	
+	//Tools
+	public static final ItemSword DRAGONS_BREATH_SWORD = new ToolSword("dragons_breath_sword", MATERIAL_DRAGONS_BREATH);
 }
---
+++ src/main/java/com/minecraftpro941/moretools/items/tools/ToolSword.java
@@ -0,0 +1,28 @@
+package com.minecraftpro941.moretools.items.tools;
+
+import com.minecraftpro941.moretools.Main;
+import com.minecraftpro941.moretools.init.ModItems;
+import com.minecraftpro941.moretools.util.IHasModels;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSword;
+
+public class ToolSword extends ItemSword implements IHasModels
+{
+	public ToolSword(String name, ToolMaterial material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MATERIALS);
+		
+		ModItems.ITEMS.add(this);
+	}
+	
+	@Override
+	public void registerModels()
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+	
+}
