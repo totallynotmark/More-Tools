e29dfe9d4c924471fdb3654500af35985d3484c9
totallynotmark <40257808+totallynotmark@users.noreply.github.com>
2018-06-15 15:34:31 -0500

    Made Version Numbers easier to read.

--- src/main/java/com/minecraftpro941/moretools/util/Reference.java
+++ src/main/java/com/minecraftpro941/moretools/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "moretools";
 	public static final String NAME = "More Tools";
-	public static final String VERSION = "1.0beta1";
+	public static final String VERSION = "1.0-beta.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.minecraftpro941.moretools.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.minecraftpro941.moretools.proxy.CommonProxy";
