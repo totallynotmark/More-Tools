fb164686dc63b3d02d7558219445fe12d5a46d3a
totallynotmark <40257808+totallynotmark@users.noreply.github.com>
2018-06-15 15:30:25 -0500

    Fix Dragon's Breath Hardness

--- src/main/java/com/minecraftpro941/moretools/init/ModItems.java
+++ src/main/java/com/minecraftpro941/moretools/init/ModItems.java
@@ -25,7 +25,7 @@ public class ModItems
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	//Materials
-	public static final ToolMaterial MATERIAL_DRAGONS_BREATH = EnumHelper.addToolMaterial("material_dragons_breath", 0, 1031, 2.0F, 5.0F, 15);
+	public static final ToolMaterial MATERIAL_DRAGONS_BREATH = EnumHelper.addToolMaterial("material_dragons_breath", 2, 1031, 2.0F, 5.0F, 15);
 	public static final ToolMaterial MATERIAL_STRUCTURE_VOID = EnumHelper.addToolMaterial("material_structure_void", 3, -1, 32.0F, 38.0F, 100);
 	//Items
 	//N/A
