91808d686214c0339916728b1aae8e6c109cc37a
totallynotmark <40257808+totallynotmark@users.noreply.github.com>
2018-06-18 14:59:16 -0500

    Sapphire Tools
    Not ready for anything yet, just setting it up for primetine.

--- src/main/java/com/minecraftpro941/moretools/init/ModItems.java
+++ src/main/java/com/minecraftpro941/moretools/init/ModItems.java
@@ -27,6 +27,8 @@ public class ModItems
 	//Materials
 	public static final ToolMaterial MATERIAL_DRAGONS_BREATH = EnumHelper.addToolMaterial("material_dragons_breath", 2, 1031, 2.0F, 5.0F, 15);
 	public static final ToolMaterial MATERIAL_STRUCTURE_VOID = EnumHelper.addToolMaterial("material_structure_void", 3, -1, 32.0F, 38.0F, 22);
+	public static final ToolMaterial MATERIAL_SAPPHIRE = EnumHelper.addToolMaterial("material_sapphire", 0, 0, 0.0F, 0.0F, 0); //TODO: add values
+
 	//Items
 	public static final Item SAPPHIRE = new ItemBase("sapphire");
 	public static final Item SAPPHIRE_ROD = new ItemBase("sapphire_rod");
@@ -42,4 +44,17 @@ public class ModItems
 	public static final ItemSpade STRUCTURE_VOID_SPADE = new ToolSpade("structure_void_spade", MATERIAL_STRUCTURE_VOID);
 	public static final ItemAxe STRUCTURE_VOID_AXE = new ToolAxe("structure_void_axe", MATERIAL_STRUCTURE_VOID);
 	public static final ItemHoe STRUCTURE_VOID_HOE = new ToolHoe("structure_void_hoe", MATERIAL_STRUCTURE_VOID);
+
+	//Sapphire
+	/*
+	public static final ItemSword SAPPHIRE_SWORD = new ToolSword("structure_void_sword", MATERIAL_SAPPHIRE);
+	public static final ItemPickaxe SAPPHIRE_PICKAXE = new ToolPickaxe("structure_void_pickaxe", MATERIAL_SAPPHIRE);
+	public static final ItemSpade SAPPHIRE_SPADE = new ToolSpade("structure_void_spade", MATERIAL_SAPPHIRE);
+	public static final ItemAxe SAPPHIRE_AXE = new ToolAxe("structure_void_axe", MATERIAL_SAPPHIRE);
+	public static final ItemHoe SAPPHIRE_HOE = new ToolHoe("structure_void_hoe", MATERIAL_SAPPHIRE);
+	*/
+
+
+
+
 }
